4e67f80babf3bf39c680218a9656996776d38b47
timhagn <timhagn@3287283.no-reply.drupal.org>
2015-10-27 20:14:00 +0800

    Issue #2413485 by timhagn: Image URL Formatter Drupal 8

--- README.txt
+++ README.txt
@@ -1,16 +1,29 @@
-This module add a url formatter for image field. 
-Then you can output image url directly.
+This module adds an FieldFormatter for an Image field,
+which let's you grab the URL of an image directly
 
-Most of the code, maybe more 90%, is just copy from the drupal core.
-I think it is stable enough.
+While most of the code comes from the core module image's
+ImageFormatter.php, some rewriting with templating has
+been done. Should be stable.
 
-Usage
-(1)After install this module,then you add an image field,
-then you can goto this bundle's manage display page,here at format column,
-you can choose "Image URL" instead of "Image".
+Usage:
+(1)
+After installation of this module you'll get a new
+format type "Image URL" you can assign to any Image Field
+in a Content Type. Just go to the Type's "Manage Display"
+and choose "Image URL" instead of "Image" from the Format
+Combo-Box.
 
-(2)When you add an iamge field in your views,
-then you config the Formatter for this field,
- here you can choose "Image URL" instead of "Image".
+(2)
+Same goes for Image Content Fields in Views, choose
+"Image URL" as format and an URL Type out of
+'Full' (default), 'Absolute' or 'Relative'.
 
-Then you can output image's URL instead of Image itself.
+Now the image's URL in the chosen form will be returned
+instead of the full <img> tag.
+
+--
+
+As there seems to be an issue with "Global: Custom Text"
+at the moment (21/08/15), just use the Template TWIG
+for formatting your output or overwrite the Template
+in your Profile.
--- src/Plugin/Field/FieldFormatter/ImageUrlFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImageUrlFormatter.php
@@ -2,38 +2,118 @@
 
 /**
  * @file
- * Contains \Drupal\image\Plugin\field\formatter\ImageFormatter.
+ * Contains \Drupal\image_url_formatter\Plugin\Field\FieldFormatter\ImageUrlFormatter.
  */
 
-namespace Drupal\image_url_formatter\Plugin\field\formatter;
+namespace Drupal\image_url_formatter\Plugin\Field\FieldFormatter;
 
-use Drupal\Component\Annotation\Plugin;
-use Drupal\Core\Annotation\Translation;
-use Drupal\field\Plugin\Type\Formatter\FormatterBase;
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'image_url' formatter.
  *
- * @Plugin(
+ * @FieldFormatter(
  *   id = "image_url",
- *   module = "image_url_formatter",
- *   label = @Translation("Image Url"),
+ *   label = @Translation("Image URL"),
  *   field_types = {
  *     "image"
- *   },
- *   settings = {
- *     "image_style" = "",
- *     "image_link" = ""
  *   }
  * )
  */
-class ImageUrlFormatter extends FormatterBase {
+class ImageUrlFormatter extends ImageFormatterBase implements ContainerFactoryPluginInterface {
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The link generator.
+   *
+   * @var \Drupal\Core\Utility\LinkGeneratorInterface
+   */
+  protected $linkGenerator;
+
+  /**
+   * The image style entity storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $imageStyleStorage;
+
+  /**
+   * Constructs an ImageFormatter object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
+   *   The link generator service.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, LinkGeneratorInterface $link_generator, EntityStorageInterface $image_style_storage) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->currentUser = $current_user;
+    $this->linkGenerator = $link_generator;
+    $this->imageStyleStorage = $image_style_storage;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(array $form, array &$form_state) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('current_user'),
+      $container->get('link_generator'),
+      $container->get('entity.manager')->getStorage('image_style')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      'url_type' => '',
+      'image_style' => '',
+      'image_link' => '',
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
     $element['url_type'] = array(
       '#title' => t('URL type'),
       '#type' => 'select',
@@ -55,14 +135,17 @@ class ImageUrlFormatter extends FormatterBase {
       '#default_value' => $this->getSetting('image_style'),
       '#empty_option' => t('None (original image)'),
       '#options' => $image_styles,
+      '#description' => array(
+        '#markup' => $this->linkGenerator->generate($this->t('Configure Image Styles'), new Url('entity.image_style.collection')),
+        '#access' => $this->currentUser->hasPermission('administer image styles'),
+      ),
     );
-
     $link_types = array(
       'content' => t('Content'),
       'file' => t('File'),
     );
     $element['image_link'] = array(
-      '#title' => t('Link image url to'),
+      '#title' => t('Link image to'),
       '#type' => 'select',
       '#default_value' => $this->getSetting('image_link'),
       '#empty_option' => t('Nothing'),
@@ -120,14 +203,23 @@ class ImageUrlFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(EntityInterface $entity, $langcode, array $items) {
-  
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
+    $files = $this->getEntitiesToView($items);
 
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+
+    $url = NULL;
     $image_link_setting = $this->getSetting('image_link');
     // Check if the formatter involves a link.
     if ($image_link_setting == 'content') {
-      $uri = $entity->uri();
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
     }
     elseif ($image_link_setting == 'file') {
       $link_file = TRUE;
@@ -135,19 +227,36 @@ class ImageUrlFormatter extends FormatterBase {
 
     $image_style_setting = $this->getSetting('image_style');
     $url_type_setting = $this->getSetting('url_type');
-    foreach ($items as $delta => $item) {
+    // Collect cache tags to be added for each item in the field.
+    $cache_tags = array();
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $cache_tags = $image_style->getCacheTags();
+    }
+
+    foreach ($files as $delta => $file) {
       if (isset($link_file)) {
-        $uri = array(
-          'path' => file_create_url($item['uri']),
-          'options' => array(),
-        );
+        $image_uri = $file->getFileUri();
+        $url = Url::fromUri(file_create_url($image_uri));
       }
+      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
       $elements[$delta] = array(
         '#theme' => 'image_url_formatter',
         '#item' => $item,
+        '#item_attributes' => $item_attributes,
         '#image_style' => $image_style_setting,
         '#url_type' => $url_type_setting,
-        '#path' => isset($uri) ? $uri : '',
+        '#url' => $url,
+        '#cache' => array(
+          'tags' => $cache_tags,
+        ),
       );
     }
 
